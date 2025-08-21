396a62f89492b83f5ec8a24ef4fa71f823159b83
Clarissa Xavier <clarissaxr@gmail.com>
2025-08-21 14:40:18 -0300

    fix: changing build path

--- src/components/forms/fields/InputText.spec.tsx
+++ src/components/forms/fields/InputText.spec.tsx
@@ -1,6 +1,5 @@
 import { expect, test } from 'vitest'
 import { render, screen } from "@testing-library/react";
-import { InputText } from "./InputText";
 
 test("renderiza o label do input", () => {
   render(<input placeholder='testing'  />);
